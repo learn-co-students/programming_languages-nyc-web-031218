68cbf90e1d7a658a8149f6f16fa549f18ef40471
Roger Perez <rogercodes1@gmail.com>
2018-02-28 06:24:05 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  array=[]
+  new_hash={}
+  languages.collect do |style, language|
+    language.collect do |name, attribute|
+      if new_hash[name]==:javascipt 
+        new_hash[name]={attribute: "interpreted", style: [:oo, :functional]}
+      else 
+        new_hash[name]={attribute: "interpreted", style: [:oo]}
+      end 
+    end
+  end
+  new_hash
+  binding.pry
 end
