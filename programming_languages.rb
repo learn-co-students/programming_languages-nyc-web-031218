c87e1e0a67c6c4d0dc559854978a5a34b45760ea
rogercodes1 <roger.ucsb@gmail.com>
2018-03-05 13:11:57 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,15 +4,19 @@ def reformat_languages(languages)
   # your code here
   array=[]
   new_hash={}
-  languages.collect do |style, language|
-    language.collect do |name, attribute|
-      if new_hash[name]==:javascipt 
-        new_hash[name]={attribute: "interpreted", style: [:oo, :functional]}
-      else 
-        new_hash[name]={attribute: "interpreted", style: [:oo]}
-      end 
+  languages.each do |style, language|
+    language.each do |name, attribute|
+      # binding.pry
+      if !new_hash.keys.include?(name)
+        new_hash[name]= attribute
+        new_hash[name][:style]=[style]
+        # binding.pry
+      else
+        new_hash[name][:style] << style
+      # binding.pry
+      end
     end
   end
   new_hash
-  binding.pry
+  # binding.pry
 end
